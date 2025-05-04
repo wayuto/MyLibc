09b3fb983fddd3d3376cbad8fa544c3742555d23
wayuto <4e07910@gmail.com>
2025-05-04 14:01:58 +0800

    Rsync

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include "include/stdio.h"
+#include "include/stdlib.h"
 
-int main(int *argc, char **argv) {
-    printf("hello world\n");
+int main(void) {
+    char var[1024];
+    printf("Variable name: ");
+    scanf("%s", &var);
+    char *value = getenv(var);
+    printf("Value: %s\n", value);
     return 0;
 }
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 echo "export LD_PATHS=$PWD/include/" >> ~/.zshrc
-echo "export SO_PATHS=$PWD/so/libio.so:$PWD/so/libfile.so" >> ~/.zshrc
+echo "export SO_PATHS=$PWD/so/libio.so:$PWD/so/libfile.so:$PWD/so/malloc.so:$PWD/so/syscall.so" >> ~/.zshrc
 echo "export LD_PATHS=$PWD/include/" >> ~/.bashrc
-echo "export SO_PATHS=$PWD/so/libio.so:$PWD/so/libfile.so" >> ~/.bashrc
+echo "export SO_PATHS=$PWD/so/libio.so:$PWD/so/libfile.so:$PWD/so/malloc.so:$PWD/so/syscall.so" >> ~/.bashrc
 gcc wcc.c -o wcc -static
 sudo ln -s $PWD/wcc /usr/bin/wcc
